79012a4ac52f866d04b9cdd3713931958696010a
Hagen Patzke <hagen-git@hpatzke.nl>
2022-10-18 13:20:56 +0200

    fixed RGB1602 driver, larger demo

--- lcd/demo/RGB1602.py
+++ lcd/demo/RGB1602.py
@@ -5,19 +5,24 @@ from machine import Pin, I2C
 RGB1602_SDA = Pin(4)
 RGB1602_SCL = Pin(5)
 
-RGB1602_I2C = I2C(0, sda=RGB1602_SDA, scl=RGB1602_SCL, freq=400000)
+# RGB1602_I2C = I2C(0, sda=RGB1602_SDA, scl=RGB1602_SCL, freq=400000)
+RGB1602_I2C = I2C(0, sda=RGB1602_SDA, scl=RGB1602_SCL, freq=125000)
 
 # Device I2C Address
-LCD_ADDRESS = (0x7c >> 1)
-RGB_ADDRESS = (0xc0 >> 1)
+LCD_ADDRESS = (0x7C >> 1)
+RGB_ADDRESS = (0xC0 >> 1)
+# for LCD1602 RGB module, I2C.Scan() shows another (unknown) I2C device
+# UNK_ADDRESS = (0xE0 >> 1)
 
-# color define
+# RGB color definitions
 REG_RED = 0x04
 REG_GREEN = 0x03
 REG_BLUE = 0x02
 REG_MODE1 = 0x00
 REG_MODE2 = 0x01
 REG_OUTPUT = 0x08
+
+# LCD commands
 LCD_CLEARDISPLAY = 0x01
 LCD_RETURNHOME = 0x02
 LCD_ENTRYMODESET = 0x04
@@ -27,27 +32,27 @@ LCD_FUNCTIONSET = 0x20
 LCD_SETCGRAMADDR = 0x40
 LCD_SETDDRAMADDR = 0x80
 
-# flags for display entry mode
-LCD_ENTRYRIGHT = 0x00
-LCD_ENTRYLEFT = 0x02
-LCD_ENTRYSHIFTINCREMENT = 0x01
-LCD_ENTRYSHIFTDECREMENT = 0x00
+# Entry Mode flags
+LCD_ENTRYRTL = 0x00  # Right-to-Left
+LCD_ENTRYLTR = 0x02  # Left-to-Right
+LCD_ENTRYSHIFT = 0x01
+LCD_ENTRYNOSHIFT = 0x00
 
-# flags for display on/off control
+# Display Control flags
 LCD_DISPLAYON = 0x04
-LCD_DISPLAYOFF = 0x00
 LCD_CURSORON = 0x02
-LCD_CURSOROFF = 0x00
 LCD_BLINKON = 0x01
+LCD_DISPLAYOFF = 0x00
+LCD_CURSOROFF = 0x00
 LCD_BLINKOFF = 0x00
 
-# flags for display/cursor shift
+# Display/Cursor Shift flags
 LCD_DISPLAYMOVE = 0x08
 LCD_CURSORMOVE = 0x00
 LCD_MOVERIGHT = 0x04
 LCD_MOVELEFT = 0x00
 
-# flags for function set
+# Function Set flags
 LCD_8BITMODE = 0x10
 LCD_4BITMODE = 0x00
 LCD_2LINE = 0x08
@@ -56,87 +61,85 @@ LCD_5x8DOTS = 0x00
 
 
 class RGB1602:
-    def __init__(self, col, row):
-        self._row = row
-        self._col = col
-
-        self._showfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
-        self.begin(self._row, self._col)
+    def __init__(self, columns=16, rows=2):
+        self._cols = columns
+        self._rows = rows
+        self._showfunction = LCD_FUNCTIONSET | LCD_4BITMODE | LCD_5x8DOTS
+        self._showfunction |= (LCD_1LINE if rows < 2 else LCD_2LINE)
+        self._showcontrol = LCD_DISPLAYCONTROL | LCD_CURSOROFF | LCD_BLINKOFF
+        self.begin()
 
     def command(self, cmd):
-        RGB1602_I2C.writeto_mem(LCD_ADDRESS, 0x80, chr(cmd))
+        RGB1602_I2C.writeto(LCD_ADDRESS, bytearray([0x80, cmd]))
 
     def write(self, data):
-        RGB1602_I2C.writeto_mem(LCD_ADDRESS, 0x40, chr(data))
+        RGB1602_I2C.writeto(LCD_ADDRESS, bytearray([0x40, data]))
 
-    def setReg(self, reg, data):
-        RGB1602_I2C.writeto_mem(RGB_ADDRESS, reg, chr(data))
+    def printout(self, arg):
+        if isinstance(arg, int):
+            arg = str(arg)
+        # bytearray(arg, 'utf-8') will make two characters from arg string/chr values 128..255
+        # x=0..127: [x], x=128..191: [0xC2, x], 192..255: [0xC3, x - 64]
+        # bytearray(arg, 'latin-1') preserves x for all chr(x) values from 0..255
+        # x=0..255: [x]
+        # RGB1602_I2C.writeto_mem(LCD_ADDRESS, 0x40, bytearray(arg, 'utf-8'))
+        RGB1602_I2C.writeto_mem(LCD_ADDRESS, 0x40, bytearray(arg, 'latin-1'))
+        return self  # enable command chaining
 
-    def setRGB(self, r, g, b):
-        self.setReg(REG_RED, r)
-        self.setReg(REG_GREEN, g)
-        self.setReg(REG_BLUE, b)
+    def set_reg(self, reg, data):
+        RGB1602_I2C.writeto(RGB_ADDRESS, bytearray([reg, data]))
 
-    def setCursor(self, col, row):
-        if (row == 0):
-            col |= 0x80;
-        else:
-            col |= 0xc0;
+    def begin(self):
+        # wait for more than 15 ms after VDD rises to 4.5V
+        time.sleep(0.050)
+        self.command(self._showfunction)
+        time.sleep(0.005)
+        self.command(self._showfunction)
+        time.sleep(0.005)
+        self.command(self._showfunction)
+        # 4th try
+        self.command(self._showfunction)
+        # turn the display on with no cursor or blinking default
+        self.display()
+        # clear it
+        self.clear()
+        # Initialize to default text direction (for roman languages)
+        self.command(LCD_ENTRYMODESET | LCD_ENTRYLTR | LCD_ENTRYNOSHIFT)
+        # RGB backlight init
+        self.set_reg(REG_MODE1, 0)
+        # set LEDs controllable by both PWM and GRPPWM registers
+        self.set_reg(REG_OUTPUT, 0xFF)
+        # set MODE2 values
+        # 0b0010_0000 -> 0x20  (DMBLNK to 1, ie blinky mode)
+        self.set_reg(REG_MODE2, 0x20)
+        self.set_color_white()
+        return self  # enable command chaining
+
+    def at(self, col, row):
+        col |= (0x80 if row < 1 else 0xC0)
         self.command(col)
+        return self  # enable command chaining
 
     def clear(self):
         self.command(LCD_CLEARDISPLAY)
         time.sleep(0.002)
+        return self  # enable command chaining
 
-    def printout(self, arg):
-        if (isinstance(arg, int)):
-            arg = str(arg)
+    def home(self):
+        self.command(LCD_RETURNHOME)
+        time.sleep(0.002)
+        return self  # enable command chaining
 
-        for x in bytearray(arg, 'utf-8'):
-            self.write(x)
+    def display(self, on=True):
+        self.command(self._showcontrol | (LCD_DISPLAYON if on else LCD_DISPLAYOFF))
+        return self  # enable command chaining
 
-    def display(self):
-        self._showcontrol |= LCD_DISPLAYON
-        self.command(LCD_DISPLAYCONTROL | self._showcontrol)
+    def set_rgb(self, r, g, b):
+        self.set_reg(REG_RED, r)
+        self.set_reg(REG_GREEN, g)
+        self.set_reg(REG_BLUE, b)
+        return self  # enable command chaining
 
-    def begin(self, cols, lines):
-        if (lines > 1):
-            self._showfunction |= LCD_2LINE
-
-        self._numlines = lines
-        self._currline = 0
-
-        time.sleep(0.05)
-
-        # Send function set command sequence
-        self.command(LCD_FUNCTIONSET | self._showfunction)
-        # delayMicroseconds(4500);  # wait more than 4.1ms
-        time.sleep(0.005)
-        # second try
-        self.command(LCD_FUNCTIONSET | self._showfunction);
-        # delayMicroseconds(150);
-        time.sleep(0.005)
-        # third go
-        self.command(LCD_FUNCTIONSET | self._showfunction)
-        # finally, set # lines, font size, etc.
-        self.command(LCD_FUNCTIONSET | self._showfunction)
-        # turn the display on with no cursor or blinking default
-        self._showcontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF
-        self.display()
-        # clear it off
-        self.clear()
-        # Initialize to default text direction (for romance languages)
-        self._showmode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT
-        # set the entry mode
-        self.command(LCD_ENTRYMODESET | self._showmode);
-        # backlight init
-        self.setReg(REG_MODE1, 0)
-        # set LEDs controllable by both PWM and GRPPWM registers
-        self.setReg(REG_OUTPUT, 0xFF)
-        # set MODE2 values
-        # 0010 0000 -> 0x20  (DMBLNK to 1, ie blinky mode)
-        self.setReg(REG_MODE2, 0x20)
-        self.setColorWhite()
-
-    def setColorWhite(self):
-        self.setRGB(255, 255, 255)
+    def set_color_white(self):
+        self.set_rgb(255, 255, 255)
+        return self  # enable command chaining
--- lcd/demo/main.py
+++ lcd/demo/main.py
@@ -1,8 +1,121 @@
 import RGB1602
+import time
 
-lcd = RGB1602.RGB1602(16, 2)
+"""
+From the LCD1602 RGB Module datasheet:
+Operating voltage: 3.3 ~ 5V,  LCD controller: AiP31068
+Communication Interface: I2C, RGB driver:     PCA9633
+Display Panel character LCD,  Display size: 64.5 × 16.0mm
+Characters: 16 × 2, Dimensions: 87.0 × 32.0 × 13.0mm
+Backlight colors: 16M, Operating temperature: -20 ~ +70℃
+"""
 
-while True:
-    lcd.printout("Welkom studenten")
-    lcd.setCursor(0, 1)
-    lcd.printout("HBO-ICT 2022 ")
+
+def define_characters(lcd):
+    heart = bytearray([0x00, 0x00, 0x1B, 0x1F, 0x1F, 0x0E, 0x04, 0x00])  # '@@[__ND@'
+    face = bytearray([0x00, 0x00, 0x0A, 0x00, 0x11, 0x0E, 0x00, 0x00])  # '@@J@QN@@'
+    lcd.command(0x40)  # custom char 0 at (0x40 | (0 << 3))
+    lcd.printout(heart)
+    lcd.command(0x48)  # custom char 1 at (0x40 | (1 << 3))
+    lcd.printout(face)
+    # Dirty trick:
+    # Because the 5x7 matrix for display only uses bits 5,4,3,2,1
+    # we can set bit 6 (0x40) and make the value a character
+    # ASCII 0x40 = '@', 0x41 = 'A', ... , 0x5F = '_'
+    lcd.command(0x50)  # custom char 2 at (0x40 | (2 << 3))
+    lcd.printout('@@[__ND@')  # heart, alternative definition
+    lcd.command(0x58)  # custom char 3 at (0x40 | (3 << 3))
+    lcd.printout('@@J@QN@@')  # face, alternative definition
+
+
+def show_r_g_b(lcd):
+    lcd.set_rgb(255, 127, 127)
+    time.sleep(1)
+    lcd.set_rgb(127, 255, 127)
+    time.sleep(1)
+    lcd.set_rgb(127, 127, 255)
+    time.sleep(1)
+    lcd.set_rgb(255, 0, 0)
+    time.sleep(1)
+    lcd.set_rgb(0, 255, 0)
+    time.sleep(1)
+    lcd.set_rgb(0, 0, 255)
+    time.sleep(1)
+
+
+def discoloration(lcd):
+    """
+    Soft color change.
+
+    Source: https://www.waveshare.com/wiki/Raspberry_Pi_Pico#LCD1602_I2C_Example
+    """
+    from math import pi, sin
+    deg2rad = pi / 180
+    t = 0
+    while t < 360:
+        r = int((abs(sin(deg2rad * t))) * 255)
+        g = int((abs(sin(deg2rad * (t + 60)))) * 255)
+        b = int((abs(sin(deg2rad * (t + 120)))) * 255)
+        lcd.set_rgb(r, g, b)
+        t = t + 3
+        time.sleep(0.1)
+
+
+def animated_banner(lcd):
+    # First line
+    target_pos = 1
+    for char in "Welkom student":
+        for ani in range(16, target_pos - 1, -1):
+            lcd.at(ani, 0).printout(char)
+            if (ani < 15):
+                lcd.at(ani + 1, 0).printout(' ')
+            time.sleep(0.1)
+        target_pos += 1
+    # Second line
+    lcd.at(2, 1)
+    for char in "HBO-ICT 2022":
+        lcd.printout(char)
+        time.sleep(0.1)
+
+
+def fade(lcd, fade_in=True):
+    for i in range(256):
+        v = i if fade_in else 255 - i
+        lcd.set_rgb(v, v, v)
+        time.sleep(0.01)
+
+
+def demo():
+    """
+    RGB1602 demonstration
+
+    Modified 2022-10-13 by Hagen Patzke, Hogeschool Utrecht HBO-ICT TI
+    """
+    lcd = RGB1602.RGB1602(16, 2)
+
+    define_characters(lcd)
+
+    while True:
+        lcd.clear()
+        fade(lcd, fade_in=True)
+        lcd.set_color_white()
+        # The RGB1602 functions return 'self' -> we can chain commands (!)
+        lcd.at(2, 0).printout(chr(0) + " WELCOME " + chr(1))
+        lcd.at(3, 1).printout(chr(1) + " STUDENT " + chr(2))
+        # "Discoloration" demo
+        discoloration(lcd)
+        # Display off for a moment
+        lcd.display(False)
+        time.sleep(1)
+        lcd.set_color_white()
+        lcd.display(True)
+        # Show second banner
+        lcd.clear()
+        animated_banner(lcd)
+        time.sleep(2)
+        show_r_g_b(lcd)
+        fade(lcd, fade_in=False)
+
+
+if __name__ == '__main__':
+    demo()
